96b23a7aeb93e4a6741677db98041c88abfc8558
Luke Freihofer <luke.freihofer@scripps.com>
2021-07-12 10:17:21 -0400

    Initial Commit

---
+++ Travis Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travis Test
+//
+//  Created by Freihofer, Luke on 7/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
